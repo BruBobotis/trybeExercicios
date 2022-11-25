0ea8ec8310ea72f8616ad9a196d9a4ddf33fd834
Bruno Alves <brunoalvesinvest@gmail.com>
2022-11-25 16:32:12 -0300

    Exercicio Extra

--- fundamentos/javaScript/dia03/script2.js
+++ fundamentos/javaScript/dia03/script2.js
@@ -40,11 +40,21 @@
 //     console.log(nova[index] / 2);
 //   };
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let lowerNumber = numbers[0];
+// for(let index = 1; index < numbers.length; index += 1){
+//     if (numbers[index] < lowerNumber){
+//         lowerNumber = numbers[index];
+//     }
+// }
+// console.log(lowerNumber);
+let array = [];
 
-let lowerNumber = numbers[0];
-for(let index = 1; index < numbers.length; index += 1){
-    if (numbers[index] < lowerNumber){
-        lowerNumber = numbers[index];
-    }
+for (let index = 0; index < numbers.length; index += 1) {
+  if (index + 1 < numbers.length) {
+    array.push(numbers[index] * numbers[index + 1]);
+  } else {
+    array.push(numbers[index] * 2);
+  }
 }
-console.log(lowerNumber);
+
+console.log(array);
