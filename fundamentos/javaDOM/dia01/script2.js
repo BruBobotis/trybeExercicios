dafd793fdac2b5d9c80d5424352d6c1bf2e022e4
Bruno Alves <brunoalvesinvest@gmail.com>
2022-12-07 16:27:22 -0300

    Exercicios javaDOM dia 1 feitos, bora pra aula VQV :rocket:

---
+++ fundamentos/javaDOM/dia01/script2.js
@@ -0,0 +1,4 @@
+let primeiroParagrafo = document.getElementsByClassName("paragraph")[0];
+primeiroParagrafo.style.color = "green";
+let subTitulo = document.getElementsByTagName("h4")[0];  
+subTitulo.style.color = "cyan";
