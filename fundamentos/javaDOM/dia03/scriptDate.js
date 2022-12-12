02d7ff380007e48e1174dbb283cc75cf9629d6e8
Bruno Alves <brunoalvesinvest@gmail.com>
2022-12-12 17:29:05 -0300

    Parte 2 Calendary

--- fundamentos/javaDOM/dia03/scriptDate.js
+++ fundamentos/javaDOM/dia03/scriptDate.js
@@ -14,7 +14,8 @@ const createDaysOfTheWeek = () => {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
-const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+//parte 1
+const decemberDaysList = [27, 28, 29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 const calendary = () => {
     const daysCalendary = document.querySelector('#days');
     for (index = 0; index < decemberDaysList.length; index += 1) {
@@ -37,3 +38,12 @@ const calendary = () => {
     }
 }
 calendary();
+//parte 2
+const holidays = (button) => {
+    const holidays = document.querySelector('.button-container');
+    let buttonHoliday = document.createElement('button');
+    buttonHoliday.innerHTML = button;
+    buttonHoliday.id = 'btn-holiday';
+    holidays.appendChild(buttonHoliday);
+}
+holidays('Feriados');
