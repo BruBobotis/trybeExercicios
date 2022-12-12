97b3c7eb10c5e307dcba353ac65559c0501adc6d
Bruno Alves <brunoalvesinvest@gmail.com>
2022-12-12 18:49:36 -0300

    Parte 5 Calendary

--- fundamentos/javaDOM/dia03/scriptDate.js
+++ fundamentos/javaDOM/dia03/scriptDate.js
@@ -15,7 +15,7 @@ createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
 //parte 1
-const decemberDaysList = [27, 28, 29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+const decemberDaysList = [27, 28, 29,'30', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 const calendary = () => {
     const daysCalendary = document.querySelector('#days');
     for (index = 0; index < decemberDaysList.length; index += 1) {
@@ -25,7 +25,7 @@ const calendary = () => {
         if (day === 24 || day === 31) {
             dayItem.className = 'day holiday';
             daysCalendary.appendChild(dayItem);
-        } else if (day === 4 || day === 11 || day === 18) {
+        } else if (day === 2 || day === 9 || day === 16 || day === 23 || day === 30) {
             dayItem.className = 'day friday';
             daysCalendary.appendChild(dayItem);
         } else if (day === 25) {
@@ -69,3 +69,30 @@ const colorHoliday = () => {
     });
 }
 colorHoliday();
+//parte 4
+const friday = (button) => {
+    const buttonFriday = document.querySelector('.buttons-container')
+    let buttonClick = document.createElement('button');
+    let buttonID = 'btn-friday';
+    buttonClick.innerHTML = button;
+    buttonClick.id = buttonID;
+    buttonFriday.appendChild(buttonClick);
+}
+friday('Sexta Feira');
+//parte 5
+const colorFriday = (event) => {
+    const dayFriday = document.getElementsByClassName('friday');
+    const buttonFriday = document.querySelector('#btn-friday');
+    let newText = 'SEXTOU!!! VQV';
+    buttonFriday.addEventListener('click', () => {
+        for (index = 0; index < dayFriday.length; index += 1) {
+            if (dayFriday[index].innerHTML === newText) {
+            dayFriday[index].innerHTML = event[index];
+            } else {
+            dayFriday[index].innerHTML = newText;
+            }
+        }
+    }) 
+}
+let december = [2, 9, 16, 23, 30]
+colorFriday(december);
